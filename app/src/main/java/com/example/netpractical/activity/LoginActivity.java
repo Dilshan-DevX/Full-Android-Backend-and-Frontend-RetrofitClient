2487b9ec419424a8be05a1806375fba60b4ae597
User <tharindu2003wxyz@gmail.com>
2026-02-13 20:21:32 +0530

    1st commit last retrofit

---
+++ app/src/main/java/com/example/netpractical/activity/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.example.netpractical.activity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.netpractical.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText emailInput = findViewById(R.id.emailInput);
+        EditText passwordInput = findViewById(R.id.passwordInput);
+        Button loginBtn = findViewById(R.id.button);
+
+
+
+
+
+
+
+    }
+}
